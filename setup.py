1db6c4f26480b4a2e41cde14a6c5254c78610230
Lulof Pirée <lulof.piree@zoho.com>
2021-11-06 19:41:13 +0100

    Setup as Python package

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name='square_roots', author="Lulof Pirée",
+      version='0.1', author_email="lulof.piree@zoho,com",
+      packages=find_packages(include=["square_roots", "square_roots.*"]))

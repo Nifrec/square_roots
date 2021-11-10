949f45f7c936f84579a438bea8ef020556bbe2b5
Lulof Pirée <lulof.piree@zoho.com>
2021-11-10 17:15:19 +0100

    Add testcases for DecimalNumber.shift()

--- test/decimal_num_test.py
+++ test/decimal_num_test.py
@@ -19,6 +19,7 @@ import unittest
 
 from square_roots.decimal_num import DecimalNumber
 
+
 class DecimalNumberConstructorTestCase(unittest.TestCase):
     """
     Test class for infinite-precision numbers.
@@ -41,7 +42,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Base case: hexagonal number.
         """
-        input_str="a.2bf"
+        input_str = "a.2bf"
         input_base = 16
         dec_num = DecimalNumber.from_string(input_str, input_base)
         result_str = str(dec_num)
@@ -52,7 +53,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Base case: negative number.
         """
-        input_str="-1.02"
+        input_str = "-1.02"
         input_base = 10
         dec_num = DecimalNumber.from_string(input_str, input_base)
         result_str = str(dec_num)
@@ -63,7 +64,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Error case: wrong base.
         """
-        input_str="1.2"
+        input_str = "1.2"
         input_base = 2
         with self.assertRaises(RuntimeError):
             DecimalNumber.from_string(input_str, input_base)
@@ -72,7 +73,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Error case: two floating points (invalid syntax).
         """
-        input_str="1.2.3"
+        input_str = "1.2.3"
         input_base = 10
         with self.assertRaises(ValueError):
             DecimalNumber.from_string(input_str, input_base)
@@ -81,7 +82,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Error case: base not supported.
         """
-        input_str="1.2"
+        input_str = "1.2"
         input_base = 40
         with self.assertRaises(NotImplementedError):
             DecimalNumber.from_string(input_str, input_base)
@@ -90,7 +91,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         """
         Error case: base not possible.
         """
-        input_str="1.2"
+        input_str = "1.2"
         input_base = 0
         with self.assertRaises(ValueError):
             DecimalNumber.from_string(input_str, input_base)
@@ -109,7 +110,7 @@ class DecimalNumberConstructorTestCase(unittest.TestCase):
         self.assertEqual(dec_num[1], 0)
         self.assertEqual(dec_num[2], 1)
         self.assertEqual(input_base, dec_num.base)
-        
+
 
 class DecimalNumberStringTestCase(unittest.TestCase):
     """
@@ -125,17 +126,16 @@ class DecimalNumberStringTestCase(unittest.TestCase):
         Base case: binary number.
         """
         decnum = DecimalNumber(2)
-        decnum[0]=1
-        decnum[1]=0
-        decnum[2]=1
-        decnum[-1]=0
-        decnum[-2]=1
+        decnum[0] = 1
+        decnum[1] = 0
+        decnum[2] = 1
+        decnum[-1] = 0
+        decnum[-2] = 1
         expected_str = "101.01"
         expected_repr = 'DecimalNumber.from_string("101.01")'
 
         self.assertEqual(str(decnum), expected_str)
         self.assertEqual(repr(decnum), expected_repr)
-
 
     def test_str_2(self):
         """
@@ -143,12 +143,12 @@ class DecimalNumberStringTestCase(unittest.TestCase):
         Zeros are added implicitly.
         """
         decnum = DecimalNumber(16)
-        decnum[5]="a"
-        decnum[1]=0
-        decnum[2]="f"
-        decnum[-1]=0
-        decnum[-2]=1
-        decnum[-3]="d"
+        decnum[5] = "a"
+        decnum[1] = 0
+        decnum[2] = "f"
+        decnum[-1] = 0
+        decnum[-2] = 1
+        decnum[-3] = "d"
         expected_str = "a00f00.01d"
         expected_repr = 'DecimalNumber.from_string("a00f00.01d")'
 
@@ -161,14 +161,15 @@ class DecimalNumberStringTestCase(unittest.TestCase):
         """
         decnum = DecimalNumber(10)
         decnum.set_sign(False)
-        decnum[0]=4
-        decnum[1]=3
-        decnum[-1]=3
+        decnum[0] = 4
+        decnum[1] = 3
+        decnum[-1] = 3
         expected_str = "-34.3"
         expected_repr = 'DecimalNumber.from_string("-34.3")'
 
         self.assertEqual(str(decnum), expected_str)
         self.assertEqual(repr(decnum), expected_repr)
+
 
 class DecimalNumberSignTestCase(unittest.TestCase):
     """
@@ -197,6 +198,57 @@ class DecimalNumberSignTestCase(unittest.TestCase):
         decnum = DecimalNumber(10, sign=True)
         decnum.set_sign(-1)
         self.assertTrue(decnum.is_negative())
+
+
+class DecimalNumberShiftTestCase(unittest.TestCase):
+    """
+    Test class for infinite-precision numbers.
+
+    This testcase focuses 'shifting' a number:
+    moving all digits one place left or right.
+    This is equivalent to multiplying or dividing 
+    over the used base respectively.
+    """
+
+    def check_shift(self, base: int, input_str: str,
+                    expected_str: str, positions: int):
+        decnum = DecimalNumber.from_string(input_str, base)
+        decnum.shift(positions)
+        result = str(decnum)
+        self.assertEqual(expected_str, result)
+
+    def test_shift_left_1(self):
+        """
+        Base case: decimal number, shift 1 position.
+        """
+        base = 10
+        positions = 1
+        input_str = "10.3"
+        expected = "103."
+        self.check_shift(base, input_str, expected, positions)
+
+    def test_shift_left_2(self):
+        """
+        Base case: binary number, shift 3 positions.
+        """
+        base = 2
+        positions = 3
+        input_str = "1.01101"
+        expected = "1011.01"
+        self.check_shift(base, input_str, expected, positions)
+
+    def test_shift_right_1(self):
+        """
+        Base case: hexadecimal number, shift -2 positions.
+        """
+        base = 16
+        positions = -2
+        input_str = "3fa.9de"
+        expected = "3.fa9de"
+        self.check_shift(base, input_str, expected, positions)
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
